4f1d345d5a9446a0ac1ea7197424bf9287ea3906
Alex2eme <159764837+Alex2eme@users.noreply.github.com>
2024-06-07 20:24:36 +0200

    Revert "Comment + Event"
    This reverts commit 11e3fd878ae5167a18343d47bfa440b9bdc4bd1d.

--- src/main/java/fr/supinfo/league/game/comment/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.supinfo.league.game.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CommentRepository extends JpaRepository<CommentEntity, UUID> {
-}
