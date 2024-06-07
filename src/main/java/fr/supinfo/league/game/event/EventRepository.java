e2ab5f46a5eeb61991b1d35962bf0241c13bd6ef
Alex2eme <159764837+Alex2eme@users.noreply.github.com>
2024-06-07 18:15:16 +0200

    StartTime, EndTime, Comment, Event
    sans les tests

---
+++ src/main/java/fr/supinfo/league/game/event/EventRepository.java
@@ -0,0 +1,8 @@
+package fr.supinfo.league.game.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<EventEntity, UUID> {
+}
