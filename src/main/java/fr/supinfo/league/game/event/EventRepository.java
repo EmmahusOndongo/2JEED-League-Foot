69c8299e016ee6a89fb8304ea13128eb832c49ed
Alex2eme <159764837+Alex2eme@users.noreply.github.com>
2024-06-07 19:43:30 +0200

    tempDelete Event

--- src/main/java/fr/supinfo/league/game/event/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.supinfo.league.game.event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EventRepository extends JpaRepository<EventEntity, UUID> {
-}
